6d8a8f03c0c931c76c3dd7f4e6d7b08f225df4c4
Maksiks <58907570+maksiksking@users.noreply.github.com>
2022-04-06 17:33:50 +0300

    Comment removal
    I removed a comment used as a note

--- IKub.py
+++ IKub.py
@@ -6,5 +6,3 @@ home = home.replace("\\", "/")
 webbrowser.open('https://aternos.org/server/')
 subprocess.run(home + "\\AppData\\Local\\Discord\\Update.exe --processStart Discord.exe")
 subprocess.run("C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe")
-
-# subprocess.run(home + "\\AppData\\Roaming\\.minecraft\\TLauncher.exe")
