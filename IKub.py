cee6981a7f30f2c6d38f4d0d9447b72df4d4ccff
Maksiks <58907570+maksiksking@users.noreply.github.com>
2022-05-10 18:11:25 +0300

    New version

--- IKub.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import webbrowser
-from pathlib import Path
-home = str(Path.home())
-home = home.replace("\\", "/")
-webbrowser.open('https://aternos.org/server/')
-subprocess.run(home + "\\AppData\\Local\\Discord\\Update.exe --processStart Discord.exe")
-subprocess.run("C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe")
