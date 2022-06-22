d922295a93b2e3b7fd97782e7851f03493d293ae
Maksiks <58907570+maksiksking@users.noreply.github.com>
2022-06-22 17:55:54 +0300

    Add files via upload

--- IKub/IKubog.py
+++ IKub/IKubog.py
@@ -1,13 +1,16 @@
 import os
 import subprocess
 import webbrowser
+import time
 from pathlib import Path
 home = str(Path.home())
 home = home.replace("\\", "/")
-webbrowser.open('https://aternos.org/server/')
 subprocess.run(home + "\\AppData\\Local\\Discord\\Update.exe --processStart Discord.exe")
+time.sleep(0.2)
 try: os.startfile (home + "\\Desktop\\Minecraft.lnk")
 except: subprocess.run("C:\\Program Files (x86)\\Minecraft Launcher\\MinecraftLauncher.exe")
+time.sleep(1.6)
+webbrowser.open('https://aternos.org/server/')
 
-# Version: 1.1 (10.05.22)
+# Version: 1.2 (Release) (10.05.22)
 # Made by Maksiks for IceCubeCraft
