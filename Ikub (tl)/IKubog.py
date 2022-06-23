3740e15642047f2e1a9efaad340bb8c268dde5f8
Maksiks <58907570+maksiksking@users.noreply.github.com>
2022-06-23 10:37:36 +0300

    1.2
    update.

--- Ikub (tl)/IKubog.py
+++ Ikub (tl)/IKubog.py
@@ -1,13 +1,17 @@
 import os
 import subprocess
 import webbrowser
+import time
 from pathlib import Path
 home = str(Path.home())
 home = home.replace("\\", "/")
-webbrowser.open('https://aternos.org/server/')
+time.sleep(0.2)
 subprocess.run(home + "\\AppData\\Local\\Discord\\Update.exe --processStart Discord.exe")
+time.sleep(0.4)
 try: os.startfile (home + "\\Desktop\\TLauncher.lnk")
 except: subprocess.run(home + "\\AppData\\Roaming\\.minecraft\\TLauncher.exe")
 
-# Version: 1.1 (10.05.22)
+webbrowser.open('https://aternos.org/server/')
+
+# Version: 1.2 (Release) (10.05.22)
 # Made by Maksiks for IceCubeCraft
